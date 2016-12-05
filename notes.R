2c50966ba9344fc4fedafbe4b9490bae78adb4e1
Jimi Damon <jdamon@accesio.com>
2016-12-04 22:14:16 -0800

    ENH: almost completed

--- notes.R
+++ notes.R
@@ -1,3 +1,15 @@
+# Measurements and key functions of performance
+# Calculating Area under Curve ( AUC )
+
+calcAUC <- function(predcol,outcol) {
+    perf <- performance(prediction(predcol,outcol==pos),'auc')
+    as.numeric(perf@y.values)
+}
+
+logLikelyhood <- function(outCol,predCol) {
+  sum(ifelse(outCol==pos,log(predCol),log(1-predCol)))
+}
+
 rule1 <- function(x){
   prediction <- rep(NA,length(x))
   prediction[x > 2.7] <- "spam"
@@ -174,7 +186,142 @@ xx <- data.frame(age=x)
 fm <- lm(wage ~ bs(age,df=3),data=training)
 predict(fm,xx)
 
-tmpdat <- data.frame(x=x,y=predict(fm,xx))
-qplot(training$age,training$wage )  + geom_line(data=tmpdat)
+tmpdat <- data.frame(age=x,wage=predict(fm,xx))
+qplot(age,wage,data=training )  + geom_line(data=tmpdat)
 # qplot(training$age,training$wage) + geom_line( data=data.frame(x=xx,y=predict(fm,xx)))
 Need to ggplot the points first
+
+
+
+# Principle component ANalysis
+library(caret); library(kernlab); data(spam)
+inTrain <- createDataPartition(y=spam$type,p=0.75, list=FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+M <- abs(cor(training[,-58]))
+
+diag(M) <- 0
+which(M > 0.8,arr.ind=T)
+
+# Modified values
+c(32 , 34 , 40 , 31 , 34 , 40 , 31 , 32 , 40 , 31 , 32 , 34 )
+
+
+# Bagging
+
+library(ElemStatLearn); data(ozone,package="ElemStatLearn")
+ozone <- ozone[order(ozone$ozone),]
+head(ozone)
+
+ozone <- ozone[order(ozone$ozone),]
+head(ozone)
+
+
+
+ll <- matrix(NA,nrow=10,ncol=155)
+for(i in 1:10){
+  ss <- sample(1:dim(ozone)[1],replace=T)
+  ozone0 <- ozone[ss,]; ozone0 <- ozone0[order(ozone0$ozone),]
+  loess0 <- loess(temperature ~ ozone,data=ozone0,span=0.2)
+  ll[i,] <- predict(loess0,newdata=data.frame(ozone=1:155))
+}
+
+
+ss <- sample(1:dim(ozone)[1],replace=T)
+
+
+# random forrests
+data(iris); library(ggplot2)
+inTrain <- createDataPartition(y=iris$Species,
+                              p=0.7, list=FALSE)
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+
+library(caret)
+modFit <- train(Species~ .,data=training,method="rf",prox=TRUE)
+modFit
+
+
+# Boosting
+
+library(ISLR); data(Wage); library(ggplot2); library(caret);
+Wage <- subset(Wage,select=-c(logwage))
+inTrain <- createDataPartition(y=Wage$wage,
+                              p=0.7, list=FALSE)
+training <- Wage[inTrain,]; testing <- Wage[-inTrain,]
+modFit <- train(wage ~ ., method="gbm",data=training,verbose=FALSE)
+
+
+
+# Model based prediction
+
+data(iris); library(ggplot2)
+names(iris)
+
+table(iris$Species)
+inTrain <- createDataPartition(y=iris$Species,
+                              p=0.7, list=FALSE)
+
+training <- iris[inTrain,]
+testing <- iris[-inTrain,]
+dim(training); dim(testing)
+
+modlda = train(Species ~ .,data=training,method="lda")
+modnb = train(Species ~ ., data=training,method="nb")
+plda = predict(modlda,testing); pnb = predict(modnb,testing)
+table(plda,pnb)
+
+
+
+
+
+# combine predictors
+
+library(ISLR); data(Wage); library(ggplot2); library(caret);
+Wage <- subset(Wage,select=-c(logwage))
+
+
+inBuild <- createDataPartition(y=Wage$wage,
+                              p=0.7, list=FALSE)
+validation <- Wage[-inBuild,]; buildData <- Wage[inBuild,]
+inTrain <- createDataPartition(y=buildData$wage,
+                              p=0.7, list=FALSE)
+training <- buildData[inTrain,]; testing <- buildData[-inTrain,]
+
+
+mod1 <- train(wage ~.,method="glm",data=training)
+mod2 <- train(wage ~.,method="rf",
+              data=training, 
+              trControl = trainControl(method="cv"),number=3)
+
+pred1 <- predict(mod1,testing); pred2 <- predict(mod2,testing)
+qplot(pred1,pred2,colour=wage,data=testing)
+
+# Combining the predictors
+
+predDF <- data.frame(pred1,pred2,wage=testing$wage)
+combModFit <- train(wage ~.,method="gam",data=predDF)
+combPred <- predict(combModFit,predDF)
+
+#combModFit <- train(wage ~.,method="gam",data=predDF)
+
+
+
+
+# Calculating errors
+
+sqrt(sum((pred1-testing$wage)^2))
+
+sqrt(sum((combPred-testing$wage)^2))
+
+
+
+
+pred1V <- predict(mod1,validation); pred2V <- predict(mod2,validation)
+predVDF <- data.frame(pred1=pred1V,pred2=pred2V)
+combPredV <- predict(combModFit,predVDF)
+
+
+
+
+
